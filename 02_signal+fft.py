6a50be5981b29bc5f2ef5ab6b593812cecc1023a
junjun <junjun.huang.de@gmail.com>
2021-06-14 16:59:03 +0200

    https://github.com/byte-jun/01_signalPro.git

---
+++ 02_signal+fft.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+#coding:utf-8
+import matplotlib.pyplot as plt
+plt.rcParams['font.sans-serif']=['SimHei'] #用来正常显示中文标签
+plt.rcParams['axes.unicode_minus']=False #用来正常显示负号
+#有中文出现的情况，需要u'内容'
+
+
+import numpy as np
+import pylab as pl
+
+sampling_rate = 8000.0
+fft_size = 512
+t = np.arange(0, 1.0, 1.0/sampling_rate)
+x = np.sin(2*np.pi*156.25*t)  + 2*np.sin(2*np.pi*234.375*t)
+xs = x[:fft_size]
+xf = np.fft.rfft(xs)/fft_size
+
+freqs = np.linspace(0.0, sampling_rate/2.0, fft_size//2+1)    
+#python2的规则中，“/”是取整，是int型
+#而在python3中，“/”符号是保留小数的，是float型
+#解决：改为“//” 即可
+
+xfp = 20*np.log10(np.clip(np.abs(xf), 1e-20, 1e100))
+pl.figure(figsize=(8,4))
+pl.subplot(211)
+pl.plot(t[:fft_size], xs)
+pl.xlabel(u"时间(秒)")
+pl.title(u"156.25Hz和234.375Hz的波形和频谱")
+pl.subplot(212)
+pl.plot(freqs, xfp)
+pl.xlabel(u"频率(Hz)")
+pl.subplots_adjust(hspace=0.4)
+pl.show()
